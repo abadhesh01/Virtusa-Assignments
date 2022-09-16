5b503ec42b8fad47f6cfac74b842cb14a9edc89c
abadhesh01 <abadheshmishra01@gmail.com>
2022-09-16 13:58:26 +0530

    16th September 2022 01:58 PM

---
+++ JAVA_Assignment(15Sept2022)/Main1.java
@@ -0,0 +1,214 @@
+
+class Colour
+{
+	public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+	public static final String WHITE_BACKGROUND = "\u001B[47m";
+	public static final String BLACK_BACKGROUND = "\u001B[40m";
+}
+
+abstract class Printer
+{
+    public abstract String print(String text, int mode, String colour);
+}
+
+class HP_Printer extends Printer
+{
+	public String print(String text, int mode, String colour)
+	{
+		if(mode == 1)
+		{
+			return  "Printing \""+ text + "\" from " + getClass().getName() + " in black and white mode.";
+		}
+		
+		if(mode == 2)
+		{
+			if(colour.toLowerCase().equals("black"))
+			   return   "Printing \""+ Colour.WHITE_BACKGROUND + Colour.BLACK + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else if(colour.toLowerCase().equals("red"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.RED + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("green"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.GREEN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("yellow"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.YELLOW + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("blue"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.BLUE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("purple"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.PURPLE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("cyan"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.CYAN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("white"))				
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.WHITE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else
+			   return "Invalid colour option selected from " + getClass().getName() + ".";
+		}
+		
+		return "Invalid printing mode selected from " + getClass().getName() + ".";
+	}
+}
+
+class Samsung_Printer extends Printer
+{
+	public String print(String text, int mode, String colour)
+	{
+		if(mode == 1)
+		{
+			return  "Printing \""+ text + "\" from " + getClass().getName() + " in black and white mode.";
+		}
+		
+		if(mode == 2)
+		{
+			if(colour.toLowerCase().equals("black"))
+			   return   "Printing \""+ Colour.WHITE_BACKGROUND + Colour.BLACK + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else if(colour.toLowerCase().equals("red"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.RED + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("green"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.GREEN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("yellow"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.YELLOW + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("blue"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.BLUE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("purple"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.PURPLE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("cyan"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.CYAN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("white"))				
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.WHITE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else
+			   return "Invalid colour option selected from " + getClass().getName() + ".";
+		}
+		
+		return "Invalid printing mode selected from " + getClass().getName() + ".";
+	}
+}
+
+class Cannon_Printer extends Printer
+{
+	public String print(String text, int mode, String colour)
+	{
+		if(mode == 1)
+		{
+			return  "Printing \""+ text + "\" from " + getClass().getName() + " in black and white mode.";
+		}
+		
+		if(mode == 2)
+		{
+		    if(colour.toLowerCase().equals("black"))
+			   return   "Printing \""+ Colour.WHITE_BACKGROUND + Colour.BLACK + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else if(colour.toLowerCase().equals("red"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.RED + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("green"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.GREEN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+			else if(colour.toLowerCase().equals("yellow"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.YELLOW + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("blue"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.BLUE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("purple"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.PURPLE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("cyan"))
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.CYAN + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+            else if(colour.toLowerCase().equals("white"))				
+			   return   "Printing \""+ Colour.BLACK_BACKGROUND + Colour.WHITE + text + Colour.RESET + "\" from " + getClass().getName() + " in colour mode.";
+		    else
+			   return "Invalid colour option selected from " + getClass().getName() + ".";
+		}
+		
+		return "Invalid printing mode selected from " + getClass().getName() + ".";
+	}
+}
+
+class Computer
+{
+	private java.util.Scanner scanner = new java.util.Scanner(System.in);
+  
+  public void printText()
+  {
+	
+	boolean flag = true;
+	while(flag)
+	{
+		try
+		{
+			System.out.print("\n[1] HP Printer\n[2] Samsung Printer\n[3] Cannon Printer\n[4] Exit\nSelect your printer: ");
+			int choice = Integer.parseInt(scanner.nextLine());
+			if(choice == 4)
+			{	
+				flag = false;
+				System.out.println("\nBye! :)\n");
+            }				
+			else if((choice < 1) || (choice > 4))
+				throw new Exception("Invalid choice !!!");
+			else
+			{
+			    System.out.print("Enter your mode('1' for black & white and '2' for colour): "); 
+				int mode = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter your text: "); 
+				String text = scanner.nextLine();
+                String colour = "Black & White";
+				if(mode == 2)
+				{
+					System.out.print("Enter text colour: ");
+					colour = scanner.nextLine();
+				}
+				switch(choice)
+				{
+					case 1: System.out.println("\n" + new HP_Printer().print(text, mode, colour) + "\n"); break;
+					case 2: System.out.println("\n" + new Samsung_Printer().print(text, mode, colour) + "\n"); break;
+					case 3: System.out.println("\n" + new Cannon_Printer().print(text, mode, colour) + "\n"); break;
+				}
+			}	
+		}
+		catch(Exception exception)
+		{
+			System.out.println("\nError: " + exception.getMessage() + "\n");
+		}
+	}
+  }	
+}
+
+public class Main1
+{
+	 //public static final String ANSI_RESET = "\u001B[0m";
+     //public static final String ANSI_BLACK = "\u001B[30m";
+     //public static final String ANSI_RED = "\u001B[31m";
+     //public static final String ANSI_GREEN = "\u001B[32m";
+     //public static final String ANSI_YELLOW = "\u001B[33m";
+     //public static final String ANSI_BLUE = "\u001B[34m";
+     //public static final String ANSI_PURPLE = "\u001B[35m";
+     //public static final String ANSI_CYAN = "\u001B[36m";
+     //public static final String ANSI_WHITE = "\u001B[37m";
+	
+	public static void main(String []args)
+	{
+		/*
+		HP_Printer printer1 = new HP_Printer();
+		Samsung_Printer printer2 = new Samsung_Printer();
+		Cannon_Printer printer3 = new Cannon_Printer();
+		
+		System.out.println("\n" + printer1.print("This is a HP printer.", 1, "Red"));
+		System.out.println(printer1.print("This is a HP printer.", 2, "Red"));
+		
+		System.out.println("\n" + printer2.print("This is a Samsung printer.", 1, "Blue"));
+		System.out.println(printer2.print("This is a Samsung printer.", 2, "Blue"));
+		
+		System.out.println("\n" + printer3.print("This is a Cannon printer.", 1, "Green"));
+		System.out.println(printer3.print("This is a Cannon printer.", 2, "Green"));
+		
+		System.out.println("\n" + printer1.print("This is a HP printer.", 3, "Red"));
+		System.out.println(printer2.print("This is a Samsung printer.", 4, "Blue"));
+		System.out.println(printer3.print("This is a Cannon printer.", 5, "Green") + "\n");
+		*/
+		
+		//System.out.println(ANSI_RED + "RED COLORED" + ANSI_RESET + " NORMAL");
+		
+		Computer computer = new Computer();
+		computer.printText();
+	}
+}
---
+++ JAVA_Assignment(15Sept2022)/Main2.java
@@ -0,0 +1,121 @@
+
+abstract class MovieTheatre
+{
+	private static double costINR; // Movie cost in rupees.
+	
+	//Setter
+	public static void setCost(double cost)
+	{
+		costINR = cost;
+	}
+	
+	//Getter
+	public static double getCost()
+	{
+		return costINR;
+	}
+	
+	//Show movie cost.
+	public static void showCost()
+	{
+		System.out.println("\nMovie cost is INR " + costINR + ".\n");
+	}
+	
+	public abstract void getTicket(double rate);
+	public abstract void watchMovie();
+}
+
+class StageTheatre extends MovieTheatre
+{
+	public void getTicket(double rate)
+	{
+		System.out.println("\nYour total bill for movie at \u001B[31m\"" + getClass().getName() + "\"\u001B[0m is \u001B[32mINR " + (getCost() + (getCost() * rate)) + "\u001B[0m.\n");
+	}
+	
+	public void watchMovie()
+	{
+		System.out.println("\nYou are now watching movie at \u001B[33m\"" + getClass().getName() + "\"\u001B[0m.\n");
+	}
+}
+
+class Multiplex extends MovieTheatre
+{
+	public void getTicket(double rate)
+	{
+		System.out.println("\nYour total bill for movie at \u001B[31m\"" + getClass().getName() + "\"\u001B[0m is \u001B[32mINR " + (getCost() + (getCost() * rate)) + "\u001B[0m.\n");
+	}
+	
+	public void watchMovie()
+	{
+		System.out.println("\nYou are now watching movie at \u001B[33m\"" + getClass().getName() + "\"\u001B[0m.\n");
+	}
+}
+
+class IMAX extends MovieTheatre
+{
+	public void getTicket(double rate)
+	{
+		System.out.println("\nYour total bill for movie at \u001B[31m\"" + getClass().getName() + "\"\u001B[0m is \u001B[32mINR " + (getCost() + (getCost() * rate)) + "\u001B[0m.\n");
+	}
+	
+	public void watchMovie()
+	{
+		System.out.println("\nYou are now watching movie at \u001B[33m\"" + getClass().getName() + "\"\u001B[0m.\n");
+	}
+}
+
+class Service
+{
+	public Service()
+	{
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		MovieTheatre.setCost(100.000);
+		MovieTheatre.showCost();
+		
+		while(true)
+		{
+			try
+			{
+				System.out.print("\n[1] StageTheatre\n[2] Multiplex\n[3] IMAX\n[4] Exit\nEnter your choice: ");
+				int choice = Integer.parseInt(scanner.nextLine());
+				if((choice < 1) || (choice > 4))
+					throw new Exception("Invalid choice !!!");
+				if(choice == 4)
+				{
+				     System.out.println("\nBye :)\n");	
+					 System.exit(0);
+				}
+				MovieTheatre movieTheatre = null;
+				double rate = 0.0;
+				switch(choice)
+				{
+					case 1: movieTheatre = new StageTheatre(); rate = 0.05; break;
+					case 2: movieTheatre = new Multiplex(); rate = 0.10; break;
+					case 3: movieTheatre = new IMAX(); rate = 0.30; break;
+				}
+				System.out.print("\n[1] Book Ticket\n[2] Watch Movie\nEnter your option: ");
+				int option = Integer.parseInt(scanner.nextLine());
+				if((option < 1) || (option > 2))
+					throw new Exception("Invalid option !!!");
+				switch(option)
+				{
+				    case 1: movieTheatre.getTicket(rate); break;
+                    case 2:	movieTheatre.watchMovie(); break;				
+				}
+			}
+			catch (Exception exception)
+			{
+				System.out.println("\nError: " + exception.getMessage() + "\n");
+			}
+		}
+	}
+}
+
+public class Main2
+{
+   public static void main(String []args)
+   {
+	   new Service();  
+   }   
+}
